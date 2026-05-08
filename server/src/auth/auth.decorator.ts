0f3fc1ef7966183c2178e91be660d764107af592
Amaury <amauryfranssen@gmail.com>
2026-05-08 17:37:17 +0200

    feat(server-auth): remove firebase from auth only

--- server/src/auth/auth.decorator.ts
+++ server/src/auth/auth.decorator.ts
@@ -1,13 +1,8 @@
 import { applyDecorators, UseGuards } from '@nestjs/common';
-import { FirebaseAuthGuard } from './firebase-auth.guard';
 import { PassportJwtAuthGuard } from './passport-jwt-auth.guard';
-import { isPassportJwtAuthEnabled } from './config/auth-env';
 
 export function Auth() {
-  const guardToUse = isPassportJwtAuthEnabled()
-    ? PassportJwtAuthGuard
-    : FirebaseAuthGuard;
   return applyDecorators(
-    UseGuards(guardToUse)
+    UseGuards(PassportJwtAuthGuard)
   );
 }
